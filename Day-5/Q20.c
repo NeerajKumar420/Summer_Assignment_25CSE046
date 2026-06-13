9520de84427679e9180b4409fa97d2c07e269c2d
NeerajKumar420 <neeraj.kumar2025@glbajajgroup.org>
2026-06-13 21:04:39 +0530

    Day-5 assignment is uploaded

---
+++ Day-5/Q20.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+    int num, largestFactor = -1;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    for (int i = 2; i <= num; ++i)
+    {
+        while (num % i == 0)
+        {
+            largestFactor = i;
+            num /= i;
+        }
+    }
+
+    if (largestFactor != -1)
+        printf("Largest prime factor is: %d\n", largestFactor);
+    else
+        printf("No prime factors found.\n");
+
+    return 0;
+}
